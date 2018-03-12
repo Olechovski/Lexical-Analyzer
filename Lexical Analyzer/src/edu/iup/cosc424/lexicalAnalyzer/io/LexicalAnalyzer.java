a88914911540aca24ef1a0f16d6797492921b3a0
Eric Olechovski <eolechovski97@gmail.com>
2018-03-11 21:38:33 -0400

    Remove test lines

--- Lexical Analyzer/src/edu/iup/cosc424/lexicalAnalyzer/io/LexicalAnalyzer.java
+++ Lexical Analyzer/src/edu/iup/cosc424/lexicalAnalyzer/io/LexicalAnalyzer.java
@@ -39,7 +39,6 @@ public class LexicalAnalyzer {
 	private static void loadLexicalAnalyzer(String fileName) throws IOException {
 		LexicalAnalyzerReader reader = new LexicalAnalyzerReader(fileName);
 		Token t;
-		int count = 0;
 		while ((t = reader.readToken()) != null) {
 			// print token here
 			if (t.type == 0){
@@ -48,9 +47,7 @@ public class LexicalAnalyzer {
 			else{
 				System.out.println("<" + t.type + "," + t.value + ">");
 			}
-			count++;
 		}
-		System.out.println(count);
 
 	}
 
